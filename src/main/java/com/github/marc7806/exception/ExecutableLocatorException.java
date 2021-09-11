bea11b17efeccd5505534247d2e7ac58146506c6
Marc <marc7806@gmx.de>
2021-09-12 00:46:10 +0200

    Remove unused method

--- src/main/java/com/github/marc7806/exception/ExecutableLocatorException.java
+++ src/main/java/com/github/marc7806/exception/ExecutableLocatorException.java
@@ -4,10 +4,6 @@ package com.github.marc7806.exception;
  * Exception that gets thrown when no executable is found
  */
 public class ExecutableLocatorException extends RuntimeException {
-    public ExecutableLocatorException(String message) {
-        super(message);
-    }
-
     public ExecutableLocatorException(String message, Throwable cause) {
         super(message, cause);
     }
